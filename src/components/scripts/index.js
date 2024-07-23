d8c9bd19d6e3a4a0dc7e903af7fe4296feb88e00
zikriramdani <zikriramdani@gmail.com>
2024-07-24 02:23:15 +0700

    comand

--- src/components/scripts/index.js
+++ src/components/scripts/index.js
@@ -14,22 +14,22 @@ const Index = () => {
       {/* <!-- jQuery Easing Plugin --> */}
       <Script src="/assets/js/vendor/easing-1.3.js" strategy="lazyOnload" />
       {/* <!-- jQuery progressbar plugin --> */}
-      <Script src="/assets/js/vendor/jquery.waypoints.min.js" strategy="lazyOnload" />
+      {/* <Script src="/assets/js/vendor/jquery.waypoints.min.js" strategy="lazyOnload" /> */}
       {/* <Script src="/assets/js/vendor/jquery.counterup.min.js" /> */}
       {/* <!-- Bootstrap Progressbar --> */}
-      <Script src="/assets/js/vendor/bootstrap-progressbar.min.js" strategy="lazyOnload" />
+      {/* <Script src="/assets/js/vendor/bootstrap-progressbar.min.js" strategy="lazyOnload" /> */}
       {/* <!-- ImagesLoaded js --> */}
-      <Script src="/assets/js/vendor/imagesloaded.pkgd.min.js" strategy="lazyOnload" />
+      {/* <Script src="/assets/js/vendor/imagesloaded.pkgd.min.js" strategy="lazyOnload" /> */}
       {/* <!-- Slick carousel js --> */}
       <Script src="/assets/js/vendor/slick.min.js" strategy="lazyOnload" />
       {/* <!-- Magnific popup --> */}
       <Script src="/assets/js/vendor/jquery.magnific-popup.min.js" strategy="lazyOnload" />
-      <Script src="/assets/js/vendor/isotope.pkgd.min.js" strategy="lazyOnload" />
+      {/* <Script src="/assets/js/vendor/isotope.pkgd.min.js" strategy="lazyOnload" /> */}
       {/* <!-- scroll magic --> */}
-      <Script src="/assets/js/vendor/jquery.ScrollMagic.min.js" strategy="lazyOnload" />
-      <Script src="/assets/js/vendor/debug.addIndicators.min.js" strategy="lazyOnload" />
-      <Script src="/assets/js/vendor/jquery.TweenMax.min.js" strategy="lazyOnload" />
-      <Script src="/assets/js/vendor/animation.gsap.min.js" strategy="lazyOnload" />
+      {/* <Script src="/assets/js/vendor/jquery.ScrollMagic.min.js" strategy="lazyOnload" /> */}
+      {/* <Script src="/assets/js/vendor/debug.addIndicators.min.js" strategy="lazyOnload" /> */}
+      {/* <Script src="/assets/js/vendor/jquery.TweenMax.min.js" strategy="lazyOnload" /> */}
+      {/* <Script src="/assets/js/vendor/animation.gsap.min.js" strategy="lazyOnload" /> */}
       {/* <Script src="/assets/js/vendor/scrollReveal.js" /> */}
       {/* <!-- Custom js --> */}
       <Script src="/assets/js/main.js" strategy="lazyOnload" />
