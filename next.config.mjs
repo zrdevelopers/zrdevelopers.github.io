f7fc346b09f756efeeab3928624ffd4defbf2179
zikriramdani <zikriramdani@gmail.com>
2025-06-30 14:47:44 +0700

    Reduce unused CSS

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/",
-  output: 'export',
   swcMinify: true,
+  output: 'export',
   images: {
     unoptimized: true,
     loader: 'akamai',
     path: ''
+  },
+  experimental: {
+    optimizeCss: true
   },
   webpack: (config) => {
     config.optimization.splitChunks = {
       chunks: 'all'
     };
     return config;
-  },
-  experimental: {
-    optimizeCss: true
   }
 };
 
